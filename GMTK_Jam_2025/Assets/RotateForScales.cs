351f388ad452ad914ad4695d70ebff0c9a13b0fb
CharlieC <jartercarter2020@gmail.com>
2025-08-01 16:33:37 +0100

    lol

--- GMTK_Jam_2025/Assets/RotateForScales.cs
+++ GMTK_Jam_2025/Assets/RotateForScales.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RotateForScales : MonoBehaviour
 {
+    private float z;
+    private bool rotateX;
+    private float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        z = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localRotation = Quaternion.Euler(0, 0, z);
     }
 }
