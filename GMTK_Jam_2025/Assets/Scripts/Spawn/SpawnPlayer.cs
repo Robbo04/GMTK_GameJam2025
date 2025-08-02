9713c872d06a4b527e775d02cec72acc06ff00f3
CharlieC <jartercarter2020@gmail.com>
2025-08-02 20:33:26 +0100

    Cube Type Counter

--- GMTK_Jam_2025/Assets/Scripts/Spawn/SpawnPlayer.cs
+++ GMTK_Jam_2025/Assets/Scripts/Spawn/SpawnPlayer.cs
@@ -6,18 +6,24 @@ using UnityEngine;
 public class SpawnPlayer : MonoBehaviour
 {
     //SerializeFields
+    [SerializeField] public GameObject spawnProtection;
     [SerializeField] private GameObject gameObjectToSpawn;
-    [SerializeField] public GameObject spawnProtection; 
+    [SerializeField] private GameObject gameObjectToSpawnBouncy;
+    [SerializeField] private GameObject gameObjectToSpawnHeavy;
+    [SerializeField] private GameObject cubeCountObject;
     //Variables
     public int livesLeft = 2;
     //Array of Cubes
     GameObject[] players;
+    public int playerType;
+    public List<int> objectType = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
         players = new GameObject[livesLeft];
         CreatePlayer();
+        
     }
 
     // Update is called once per frame
@@ -28,7 +34,7 @@ public class SpawnPlayer : MonoBehaviour
             if (!spawnProtection.GetComponent<SpawnProtection>().IsInside)
             {
                 CreatePlayer();
-                
+                //if ()
                 //print(spawnProtection.GetComponent<SpawnProtection>().IsInside);
             }
         }
@@ -36,8 +42,25 @@ public class SpawnPlayer : MonoBehaviour
 
     public void CreatePlayer()
     {
-        players[livesLeft - 1] = Instantiate(gameObjectToSpawn, gameObject.transform.localPosition, Quaternion.identity) as GameObject;
-        livesLeft--;
+        
+        switch (objectType[0])
+        {
+            case 0:
+                players[livesLeft - 1] = Instantiate(gameObjectToSpawn, gameObject.transform.localPosition, Quaternion.identity) as GameObject;
+                livesLeft--;
+                break;
+            case 1:
+                players[livesLeft - 1] = Instantiate(gameObjectToSpawnBouncy, gameObject.transform.localPosition, Quaternion.identity) as GameObject;
+                livesLeft--;
+                break;
+            case 2:
+                players[livesLeft - 1] = Instantiate(gameObjectToSpawnHeavy, gameObject.transform.localPosition, Quaternion.identity) as GameObject;
+                livesLeft--;
+                break;
+            default:
+                print("Error");
+                break;
+        }
+        cubeCountObject.GetComponent<cubeCount>().RemoveNext();
     }
-
 }
