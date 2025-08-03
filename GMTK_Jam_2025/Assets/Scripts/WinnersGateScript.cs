03f42ca67f02e8c182940ea486dd5f1917dc83dc
CharlieC <jartercarter2020@gmail.com>
2025-08-03 17:14:56 +0100

    WinMenuFixed

--- GMTK_Jam_2025/Assets/Scripts/WinnersGateScript.cs
+++ GMTK_Jam_2025/Assets/Scripts/WinnersGateScript.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class WinnersGateScript : MonoBehaviour
 {
     [SerializeField] AudioClip CoinCollect;
+    [SerializeField] public GameObject retryButton;
+    [SerializeField] public GameObject winScreen;
+
+
+    void Start()
+    {
+        winScreen.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("Level Complete");
+        retryButton.SetActive(false);
+        winScreen.SetActive(true);
         //SoundFXManager.instance.PlaySoundFXCLip(CoinCollect, transform, 1f);
         Destroy(this.gameObject);
     }
